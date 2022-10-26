55b097a013d7ef4cf418385c01dc8f449646f4e6
Nastasya2141 <113186212+Nastasya2141@users.noreply.github.com>
2022-10-26 20:07:25 +0300

    FinalTaskCode

---
+++ FinalTask/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+длина которых меньше, либо равна 3 символам.
+Примеры:
+[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+[“Russia”, “Denmark”, “Kazan”] → []
+*/
+
+
+
+string[] FirstArray= new string[4] {"Last", "26", "Task", "10"};
+string[] SecondArray = new string[FirstArray.Length];
+FinalArray(FirstArray, SecondArray);
+PrintArray(SecondArray);
+
+
+
+
+void FinalArray(string[] FirstArray, string[] SecondArray)
+{
+    int count = 0;
+    for (int i = 0; i < FirstArray.Length; i++)
+    {
+    if(FirstArray[i].Length <= 3)
+        {
+        SecondArray[count] = FirstArray[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
